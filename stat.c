6ba8721513b97a8fa2a7679a595dd916b3f50bb7
Nikhiljomon05 <nikhilaerathu@gmail.com>
2025-02-11 13:48:37 +0530

    Add files via upload

--- stat.c
+++ stat.c
@@ -5,17 +5,19 @@
 #include<sys/stat.h>
 void main()
 {
-char*path,path1[10];
-struct stat*nfile;
-nfile=(struct stat*)malloc(sizeof(struct stat));
-printf("enter the file Name");
-scanf("%s",path1);
-stat(path1,nfile);
-printf("user ID:%d\n",nfile->st_uid);
-printf("Block size:%ld\n",nfile->st_blksize);
-printf("last accesss time:%ld\n",nfile->st_atime);
-printf("Last modification time:%ld\n",nfile->st_mtime);
-printf("Production mode:%d\n",nfile->st_mode);
-printf("Size of file:%ld\n",nfile->st_size);
-printf("Number of links:%ld\n",nfile->st_nlink);
+  char *path,path1[10];
+  struct stat *nfile;
+  nfile=(struct stat*)malloc(sizeof(struct stat));
+  printf("Enter the file name:");
+  scanf("%s",path1);
+  stat(path1,nfile);
+  printf("User id:%d\n",nfile->st_uid);
+  printf("block size:%ld\n",nfile->st_blksize);
+  printf("product mode:%d\n",nfile->st_mode);
+  printf("access time:%ld\n",nfile->st_atime);
+  printf("Last modified time:%ld\n",nfile->st_mtime);
+  printf("size of file:%ld\n",nfile->st_size);
+  printf("Number of links:%ld\n",nfile->st_nlink);
 }
+	
+
